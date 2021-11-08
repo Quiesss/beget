2c72e5521ac6d5de9d828011bec6499eb255ba3d
Igor <igorshmal38@gmail.com>
2021-11-08 12:06:50 +0300

    show privatflare to user, not 'sss' again

--- index.php
+++ index.php
@@ -182,7 +182,7 @@ $app->post('/uploadDomains', function (Request $request, Response $response) use
 $app->get('/privateflare', function (Request $request, Response $response) use ($view, $session) {
     $userLogin = $session->getData('user')['user_login'];
     $dFromPF = new privatflare();
-    $viewDomains = $dFromPF->getDomens(1, 'sss');
+    $viewDomains = $dFromPF->getDomens(1, $userLogin);
     $body = $view->render('pf.twig', [
         'domenlist' => $viewDomains,
         'msg' => $session->flush('msg'),
